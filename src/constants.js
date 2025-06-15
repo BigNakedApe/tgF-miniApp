10244139cc8983f33ac73df92916acfe944e9dce
BigNakedApe <eosfloyd0@gmail.com>
2025-06-15 12:49:59 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,10 @@
 export const currencies = [
-  { value: 'usd', label: '💵 USD', apiValue: 'usdt' },
-  { value: 'stars', label: '⭐ Stars', apiValue: 'stars' }
+  { value: 'usdt', label: 'USDT', apiValue: 'usdt' },
+  { value: 'ton', label: 'TON', apiValue: 'ton' },
+  { value: 'btc', label: 'BTC', apiValue: 'btc' },
+  { value: 'eth', label: 'ETH', apiValue: 'eth' },
+  { value: 'ltc', label: 'LTC', apiValue: 'ltc' },
+  { value: 'bnb', label: 'BNB', apiValue: 'bnb' },
+  { value: 'trx', label: 'TRX', apiValue: 'trx' },
+  { value: 'usdc', label: 'USDC', apiValue: 'usdc' }
 ];

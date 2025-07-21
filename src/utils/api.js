547c2fe05f945d48efec2fb9c198ff49d76582eb
BigNakedApe <eosfloyd0@gmail.com>
2025-07-21 13:32:32 +0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const API_URL = import.meta.env.VITE_API_URL || process.env.VITE_API_URL || 'http://backend:8000';
 
 export const getUserByTelegramId = async (telegramId) => {
   try {
